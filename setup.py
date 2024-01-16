2bc7f03494560224649c39c46827a42b123e999b
Jeremy Compostella <jeremy.compostella@intel.com>
2024-01-16 13:55:29 -0700

    Version 0.2
    Signed-off-by: Jeremy Compostella <jeremy.compostella@intel.com>

--- setup.py
+++ setup.py
@@ -1,23 +1,22 @@
 import setuptools
 
-try:
-    with open("README.md", "r") as readme:
-        long_description = readme.read()
-except:
-    long_description = ""
+with open('README.md', 'r') as readme:
+    long_description = readme.read()
 
 setuptools.setup(
-    name="aquanta",
-    version="0.1",
-    author="Jeremy Compostella",
-    description="Unofficial library to interact with Aquanta water heater smart controller.",
+    name='aquanta',
+    version='0.2',
+    author='Jeremy Compostella',
+    author_email='jeremy.compostella@gmail.com',
+    description='Unofficial library to interact with Aquanta water heater smart controller.',
     long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/jeremy-compostella/aquanta",
-    packages=setuptools.find_packages(),
-    classifiers=["Programming Language :: Python :: 3",
-                 "License :: OSI Approved :: BSD License",
-                 "Operating System :: OS Independent",
-                 "Topic :: Home Automation"],
-    python_requires='>=3.4',
+    long_description_content_type='text/markdown',
+    url='https://github.com/jeremy-compostella/aquanta',
+    classifiers=['Programming Language :: Python :: 3',
+                 'License :: OSI Approved :: BSD License',
+                 'Operating System :: OS Independent',
+                 'Topic :: Home Automation'],
+    package_dir={'': 'src'},
+    packages=setuptools.find_packages(where='src'),
+    python_requires='>=3.6',
     install_requires=['requests'])
